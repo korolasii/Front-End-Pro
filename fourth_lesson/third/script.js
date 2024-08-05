2a9225de269c92e90654ec69ed239c632864b61b
korolasii <timofei.kropachev@gmail.com>
2024-08-05 22:03:12 +0300

    add_home_work_lesson_4_third

--- fourth_lesson/third/script.js
+++ fourth_lesson/third/script.js
@@ -1,26 +1,36 @@
-
-let userYearOfBirth = prompt("Your year of birth: "),
+let userYearOfBirth = prompt("Your year of birth: ");
 currentYear = new Date().getFullYear(),
-userAge = currentYear - userYearOfBirth,
-userCity = prompt("Your name of city: "),
-userFavoriteSport = prompt("Your favorite sport: ");
+userAge = userYearOfBirth !== null && userYearOfBirth.trim() !== "" ? currentYear - userYearOfBirth : null,
+userFavoriteSport = prompt("Your favorite sport: "),
+userCity = prompt("Your name of city: ");
 
-if (userYearOfBirth !== null && userYearOfBirth.trim() !== "") {
+if (userYearOfBirth === null) {
+    alert("Шкода, що Ви не захотіли ввести свою дату народження.");
+} else if (userYearOfBirth.trim() !== "") {
     alert(`Your age: ${userAge}`);
 }
 
-switch (userCity.toLowerCase().trim()) {
-    case "київ":
-        alert("Ти живеш у столиці України.");
-        break;
-    case "вашингтон":
-        alert("Ти живеш у столиці США.");
-        break;
-    case "лондон":
-        alert("Ти живеш у столиці Великобританії.");
-        break;
-    default:
-        alert(`Ти живеш у місті ${userCity}.`);
-        break;
+if (userCity === null) {
+    alert("Шкода, що Ви не захотіли ввести своє місто.");
+} else {
+    switch (userCity.toLowerCase().trim()) {
+        case "київ":
+            alert("Ти живеш у столиці України.");
+            break;
+        case "вашингтон":
+            alert("Ти живеш у столиці США.");
+            break;
+        case "лондон":
+            alert("Ти живеш у столиці Великобританії.");
+            break;
+        default:
+            alert(`Ти живеш у місті ${userCity}.`);
+            break;
+    }
 }
 
+if (userFavoriteSport === null) {
+    alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
+} else if (sportsChampions.hasOwnProperty(userFavoriteSport.toLowerCase().trim())) {
+    alert(`Круто! Хочеш стати ${sportsChampions[userFavoriteSport.toLowerCase().trim()]}?`);
+}
