439969035f8a8220d344a17762670e9136f4294f
korolasii <timofei.kropachev@gmail.com>
2024-08-12 20:28:15 +0300

    add_home_work_lesson_5_first

---
+++ fifrh_lesson/first/script.js
@@ -0,0 +1,110 @@
+//the first thing that came to mind
+let numberToConsole = 19.5;
+
+while (numberToConsole<30) {
+    console.log(numberToConsole += 0.5);
+}
+
+// console.log("----------")
+
+//the second option is more detailed
+// let numberToConsole2 = 20;
+
+// while (numberToConsole2<=30) {
+//     console.log(numberToConsole2);
+//     numberToConsole2 += 0.5
+// }
+
+// console.log("----------")
+
+//the first thing that came to mind with do-while
+// let numberToConsole3 = 19.5;
+
+// do {
+//     console.log(numberToConsole3 += 0.5);
+// } while (numberToConsole3 < 30);
+
+// console.log("----------")
+
+//the second option with do-while is more detailed 
+// let numberToConsole4 = 20;
+
+// do {
+//     console.log(numberToConsole4);
+//     numberToConsole4 += 0.5
+// } while (numberToConsole4 <= 30);
+
+// console.log("----------")
+
+//the first thing that came to mind with for (I think this is the simplest and most effective option)
+// for (let numberToConsole5 = 20; numberToConsole5 <= 30; numberToConsole5 += 0.5) {
+//     console.log(numberToConsole5)
+// }
+
+// console.log("----------")
+
+//option with an endless loop (I don’t see the point in this, but it’s a possible option)
+// let numberToConsole6 = 19.5;
+
+// while (true) {
+//     if (numberToConsole6 < 30) {
+//         console.log(numberToConsole6 += 0.5)
+//     }else{
+//         break;
+//     }
+// }
+
+// console.log("----------")
+
+//the second option is more detailed with an endless loop (I don’t see the point in this, but it’s a possible option)
+// let numberToConsole7 = 20;
+
+// while (true) {
+//     if (numberToConsole7 <= 30) {
+//         console.log(numberToConsole7)
+//         numberToConsole7 += 0.5
+//     }else{
+//         break;
+//     }
+// }
+
+// console.log("----------")
+
+//option with an endless loop with do-wile (I don’t see the point in this, but it’s a possible option)
+// let numberToConsole8 = 19.5;
+
+// do {
+//     if(numberToConsole8 < 30){
+//         console.log(numberToConsole8 += 0.5);
+//     }else{
+//         break;
+//     }
+// } while (true);
+
+// console.log("----------")
+
+//the second option is more detailed with an endless loop with do-wile (I don’t see the point in this, but it’s a possible option)
+// let numberToConsole8 = 20;
+
+// do {
+//     if(numberToConsole8 <= 30){
+//         console.log(numberToConsole8);
+//         numberToConsole8 += 0.5
+//     }else{
+//         break;
+//     }
+// } while (true);
+
+// console.log("----------")
+
+//option with an endless loop with for (I don’t see the point in this, but it’s a possible option)
+// for (let numberToConsole9 = 20; true; numberToConsole9 += 0.5) {
+//     if (numberToConsole9 <= 30) {
+//         console.log(numberToConsole9)
+//     }else{
+//         break;
+//     }
+// }
+
+
+//I couldn't think of any more options
