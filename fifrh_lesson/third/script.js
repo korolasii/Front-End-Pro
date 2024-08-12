d7644c1698bfdc1b4f75a4d28132c374e8629b5d
korolasii <107001703+korolasii@users.noreply.github.com>
2024-08-12 21:59:59 +0300

    Update script.js

--- fifrh_lesson/third/script.js
+++ fifrh_lesson/third/script.js
@@ -2,7 +2,7 @@ let maxNumber = prompt('Enter max number:');
 let startValue = 1;
 let endValue = 100;
 
-for (let number = startValue; number <=100 endValue; number++) {
+for (let number = startValue; number <= endValue; number++) {
     let numberToThePower = number**2;
     if (numberToThePower <= maxNumber) {
         console.log(numberToThePower);
