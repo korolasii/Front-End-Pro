ddc6108d671445b78d46db80c93c6a0b8fc81063
korolasii <timofei.kropachev@gmail.com>
2024-08-12 21:58:35 +0300

    add_home_work_lesson_5_third

---
+++ fifrh_lesson/third/script.js
@@ -0,0 +1,12 @@
+let maxNumber = prompt('Enter max number:');
+let startValue = 1;
+let endValue = 100;
+
+for (let number = startValue; number <=100 endValue; number++) {
+    let numberToThePower = number**2;
+    if (numberToThePower <= maxNumber) {
+        console.log(numberToThePower);
+    }else{
+        break;
+    }
+}
