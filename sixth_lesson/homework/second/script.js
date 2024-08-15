b0d2d479bf39634bcb80a7ab5dc11707d0562e75
korolasii <timofei.kropachev@gmail.com>
2024-08-15 22:04:05 +0300

    add_home_work_lesson_6_second

---
+++ sixth_lesson/homework/second/script.js
@@ -0,0 +1,23 @@
+function average(array = []){
+    let sum = 0;1, 2
+    let count = 0;
+    let averageArray = 0;
+    for (let i = 0; i < array.length; i++) {
+
+        if (!isNaN(array[i]) && Number.isInteger(Number(array[i]))) {
+            sum += Number(array[i]);
+            count++;
+        }
+    }
+
+    averageArray = count > 0 ? sum / count : 0;
+    return averageArray
+}
+
+let stringToAverage = prompt("Enter data separated by commas(1, 2, 'none', 52): ")
+let arrayToAverage = stringToAverage.split(", ")
+
+let resultAverage = average(arrayToAverage);
+
+console.log(resultAverage);
+
